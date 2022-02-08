d3ef7b3fadd79fa38e8edf29f051941226aba855
Pavel Pleshko <pavel.pleshko@sbgenomics.com>
2022-02-08 12:06:55 +0100

    test(accrued interest): add tests for interests calculation and helpers

--- test/test-helpers/interest-rates-calculations.ts
+++ test/test-helpers/interest-rates-calculations.ts
@@ -4,6 +4,13 @@ import { rayFactor, SECONDS_ONE_YEAR } from '../../utils/constants';
 import { FloatMath, roundDown } from '../misc/ray-math-calculations';
 
 
+export const calculateSimpleInterest = (rate: BigNumber, lastUpdateTime: number, currentTime: number): BigNumber => {
+    const timeDelta = new BigNumber(currentTime - lastUpdateTime);
+    const accumulated = rate.multipliedBy(timeDelta).dividedBy(SECONDS_ONE_YEAR);
+
+    return roundDown(accumulated).plus(rayFactor);
+};
+
 export const calculateCompoundedInterest = (rate: BigNumber, lastUpdateTime: number, currentTime: number): BigNumber => {
     const timeDelta = new BigNumber(currentTime - lastUpdateTime);
     const accumulationSpeedPerSecond = roundDown(rate.div(SECONDS_ONE_YEAR));
@@ -11,6 +18,10 @@ export const calculateCompoundedInterest = (rate: BigNumber, lastUpdateTime: num
     return FloatMath.rPow(accumulationSpeedPerSecond.plus(rayFactor), timeDelta);
 };
 
-export const calculateNewBorrowRate = (accruedInterest: BigNumber, currentBorrowRate: BigNumber): BigNumber => {
-    return FloatMath.rMul(accruedInterest, currentBorrowRate);
+export const calculateNewBorrowIndex = (accruedInterest: BigNumber, currentBorrowIndex: BigNumber): BigNumber => {
+    return FloatMath.rMul(accruedInterest, currentBorrowIndex);
+};
+
+export const calculateNewLiquidityIndex = (accruedInterest: BigNumber, currentLiquidityIndex: BigNumber): BigNumber => {
+    return FloatMath.rMul(accruedInterest, currentLiquidityIndex);
 };
