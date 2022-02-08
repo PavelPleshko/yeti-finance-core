041a0db61efb314e9465ea5c134ed7b5a85fa03b
Pavel Pleshko <pavel.pleshko@sbgenomics.com>
2022-02-08 15:34:07 +0100

    refactor: use more robust way to get the current block for unit tests

--- test/misc/evm-commands.ts
+++ test/misc/evm-commands.ts
@@ -3,11 +3,10 @@ import { DEV_RE } from '../../utils/misc';
 
 
 export const travelToFuture = async (seconds: number): Promise<void> => {
-    await DEV_RE.ethers.provider.send('evm_increaseTime', [seconds]);
+    await DEV_RE.ethers.provider.send('evm_increaseTime', [ seconds ]);
     await DEV_RE.ethers.provider.send('evm_mine', []);
 };
 
 export const getCurrentBlock = async (): Promise<Block> => {
-    const blockNumBefore = await DEV_RE.ethers.provider.getBlockNumber();
-    return await DEV_RE.ethers.provider.getBlock(blockNumBefore);
+    return await DEV_RE.ethers.provider.getBlock('latest');
 };
